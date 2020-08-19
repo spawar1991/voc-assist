95afb6265e2caf315f5073623eade7a336db19cb
spawar1991 <spawar@hgsdigital.com>
2020-08-19 14:47:14 +0530

    tf files

--- basic-dynamodb-table.tf
+++ basic-dynamodb-table.tf
@@ -41,6 +41,16 @@ resource "aws_dynamodb_table" "basic-dynamodb-table" {
     enabled        = false
   }
 
+  global_secondary_index {
+    name               = "GameTitleIndex"
+    hash_key           = "GameTitle"
+    range_key          = "TopScore"
+    write_capacity     = 10
+    read_capacity      = 10
+    projection_type    = "INCLUDE"
+    non_key_attributes = ["UserId"]
+  }
+  
   tags = {
     Name        = "dynamodb-terraform"
     Environment = "production"
